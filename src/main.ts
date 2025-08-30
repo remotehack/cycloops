720555fab4844b51b73a3c8f12965bdb10121b0b
Ryan Brooks <ryan@slatehorse.com>
2025-08-30 10:57:22 +0100

    capture input

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import './style.css'
 
+const form = document.querySelector('form')
+const posts = document.querySelector('ol')
 
+form?.addEventListener('submit', e => {
+  e.preventDefault()
 
+  const data = new FormData(e.target)
+  console.log("d", data.get('message'))
+
+})
